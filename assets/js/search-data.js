19e6d5df848f3e2a32a997b820b974d88fa5536a
martinez-lopezpablo <133849651+martinez-lopezpablo@users.noreply.github.com>
2025-12-02 10:35:00 +0000

    Deploying to gh-pages from @ martinez-lopezpablo/martinez-lopezpablo.github.io@cd5bbfab7b3f8c1589cbf69192aa763390a0b845 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-presentations",
+          title: "presentations",
+          description: "Collection of my presentations and posters at conferences.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -28,24 +28,24 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-sepc2023",
+          title: 'SEPC2023',
+          description: "Poster",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-sepex2024",
+          title: 'SEPEX2024',
+          description: "1. Oral comm. at a symposium. 2. Poster",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-eam2025",
+          title: 'EAM2025',
+          description: "Oral comm.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-sepc2025",
+          title: 'SEPC2025',
+          description: "Oral comm. at a symposium",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
