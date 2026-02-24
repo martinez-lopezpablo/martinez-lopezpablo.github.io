9b302f9b207370c5ab5574f67fd66301834c06c6
martinez-lopezpablo <133849651+martinez-lopezpablo@users.noreply.github.com>
2026-02-24 18:41:21 +0000

    Deploying to gh-pages from @ martinez-lopezpablo/martinez-lopezpablo.github.io@d8ac44cbe6bed5695761b34fbe9cdc92e101fe52 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-flare",
+          title: "FLARE",
+          description: "Until now, all my outreach activity has been in Spanish. Sorry!",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/outreach/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
